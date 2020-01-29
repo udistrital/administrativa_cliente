02753fbbf92cb1f601ead939c25d118c08550396
blacburn <andreysa0114@hotmail.com>
2020-01-28 19:42:08 -0500

    Update aprobacion_supervisor.js
    actualizacion para filtro año

--- app/scripts/controllers/seguimientoycontrol/tecnico/aprobacion_supervisor.js
+++ app/scripts/controllers/seguimientoycontrol/tecnico/aprobacion_supervisor.js
@@ -71,7 +71,7 @@ angular.module('contractualClienteApp')
     ];
 
     self.d = new Date();
-    self.anios = [(self.d.getFullYear()), (self.d.getFullYear() + 1)];
+    self.anios = [(self.d.getFullYear()), (self.d.getFullYear() + 1), (self.d.getFullYear() - 1)];
 
         /*
       Función para obtener la imagen del escudo de la universidad
